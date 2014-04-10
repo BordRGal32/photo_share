97d6099c67d9dcd48a0717cb47945214ceda1987
Lauren Yeiser & Maureen Dugan <student@epicodus.com>
2014-04-10 15:01:38 -0700

    update photo class model for image size

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
 
   belongs_to :user
   has_many :tags
-  has_attached_file :image
+  has_attached_file :image, :styles =>{:medium => "300x300>"}
   validates_attachment_content_type :image,
                                     :content_type => /^image\/(png|gif|jpeg)/,
                                     :message => 'only (png/gif/jpeg) images'
