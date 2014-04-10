eb8b2b76088e04566e0d55042f67d2c0ceab8f6a
Lauren Yeiser & Maureen Dugan <student@epicodus.com>
2014-04-10 09:26:55 -0700

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,8 @@ get 'login', to: 'sessions#new', as: 'login'
 get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :sessions
-
-    resources :users, :except => [:delete] do
-      resources :photos do
-        resources :tags
-      end
-    end
+  resources :users
+  resources :photos
+  resources :tags
 
 end
